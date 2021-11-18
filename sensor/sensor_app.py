f0644e7fb25fe9914668d39c2616a564ba11a2a0
MatthewReddy0 <MReddy@spartaglobal.com>
2021-11-18 15:36:12 +0000

    Module 1

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,5 @@
 # Runner script for all modules
-
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
@@ -9,11 +9,27 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 
+"""
 # Module 2 code here:
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print("\nHouse sensor records for area {} = {} ".format(test_area, len(recs)))
+
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_date("id", rec_date=test_date)
+print("House sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+temperature_data = TemperatureDate(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
 
 # Module 5 code here:
+"""
